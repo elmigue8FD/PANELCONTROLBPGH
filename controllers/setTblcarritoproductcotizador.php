e238fd9740ac6558b72ca3bc9cf7925703082780
MisaelBravo <misaelbbravo@gmail.com>
2016-12-07 14:53:01 -0600

    Actualizacion

--- controllers/setTblcarritoproductcotizador.php
+++ controllers/setTblcarritoproductcotizador.php
@@ -14,29 +14,26 @@ $srcimgproducto = '';
 $idtblordencotizador = '';
 $idtblproductcotizador = '';
 $emailcreo      = '';
+$idtblmotivocotizacion = '';
 $resultado      = '';
 /**
  * Validamos que el array $_POST no es null.
  */
 if (!empty($_POST)){
-	$solicitadoBy           = $_POST["solicitadoBy"];
-	$numpersonas	        = $_POST['numpersonas'];
-	$fchentrega             = $_POST['fchentrega'];
-	$srcimgproducto         = $_POST['srcimgproducto'];
+    $solicitadoBy           = $_POST["solicitadoBy"];
+    $numpersonas            = $_POST['numpersonas'];
+    $fchentrega             = $_POST['fchentrega'];
+    $srcimgproducto         = $_POST['srcimgproducto'];
     $idtblordencotizador    = $_POST['idtblordencotizador'];
     $idtblproductcotizador  = $_POST['idtblproductcotizador'];
-	$emailcreo              = $_POST['emailcreo'];
+    $emailcreo              = $_POST['emailcreo'];
+    $idtblmotivocotizacion  = $_POST['idtblmotivocotizacion'];
     /**
-     * Validamos que el pais no este registrado
+     * 
      */
     
-    $resultado = FuncionesBePickler::setCheckTblcarritoproductcotizador($idtblordencotizador,$idtblproductcotizador);
-    /**
-     * Mandamos los parámetros y llamamos a la función que ejecutara la sentencia y retorna el resultado.
-     */
-    if($resultado==0)
-    {
-        $resultado = FuncionesBePickler::setTblcarritoproductcotizador($numpersonas, $fchentrega, $srcimgproducto, $idtblordencotizador, $idtblproductcotizador, $emailcreo);
+   
+        $resultado = FuncionesBePickler::setTblcarritoproductcotizador($numpersonas, $fchentrega, $srcimgproducto, $idtblordencotizador, $idtblproductcotizador, $emailcreo, $idtblmotivocotizacion);
         if($resultado){
             /**
              * Si es éxitos le mandamos los resultados a quien lo solicito.
@@ -48,10 +45,7 @@ if (!empty($_POST)){
              * Si fallo manda a la función de fallo a quien lo solicito.
              */
             InfoSolicitadaBy::sinDatos($solicitadoBy);
-        }  
-    }else{
-        InfoSolicitadaBy::registroRepetido($solicitadoBy);
-    }    
+        }     
 }
 /**
  * Desctruimos las variables para liberar memoria
@@ -63,6 +57,7 @@ unset($srcimgproducto);
 unset($idtblordencotizador);
 unset($idtblproductcotizador);
 unset($emailcreo);
+unset($idtblmotivocotizacion);
 unset($resultado);
 unset($_POST);
 ?>
