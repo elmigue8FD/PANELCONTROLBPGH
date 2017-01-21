95d6e32545a8e4ccfa1f35fd8574dccef4b7362b
MisaelBravo <misaelbbravo@gmail.com>
2017-01-20 18:03:19 -0600

    controller de los rangoshorario

---
+++ controllers/getRangoshorarios.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Recursos utilizados
+ */
+require './../models/FuncionesBePickler.php';
+require './InfoSolicitadaBy.php';
+/**
+ * Variables Utilizadas
+ */
+$solicitadoBy= '';
+$fechapedido= '';
+$resultado='';
+/**
+ * Validamos que el array $_POST no es null.
+ */
+if (!empty($_POST)){
+
+	$solicitadoBy=$_POST["solicitadoBy"];
+    $fechapedido=$_POST["fecha"];
+    /**
+     * Mandamos los parámetros y llamamos a la función que ejecutara la sentencia y retorna el resultado.
+     */
+    $resultado = FuncionesBePickler::getRangoshorarios($fechapedido);
+
+    if($resultado)
+    {
+        /**
+         * Si es éxitos le mandamos los resultados a quien lo solicito.
+         */
+    	InfoSolicitadaBy::solicitadaby($solicitadoBy, $resultado);
+
+    }else
+    {
+        /**
+         * Si fallo manda a la función de fallo a quien lo solicito.
+         */
+    	InfoSolicitadaBy::sinDatos($solicitadoBy);
+    }
+}
+/**
+ * Desctruimos las variables para liberar memoria
+ */
+unset($solicitadoBy);
+unset($idtblcliente);
+unset($resultado);
+?>
