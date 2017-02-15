a6a0078f8f868806b2241f5dae1f73db84704a14
MisaelBravo <misaelbbravo@gmail.com>
2017-02-14 23:35:27 -0600

    Nuevo controller funcion

---
+++ controllers/setUpdateStockTblproductdetalle.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Recursos utilizados
+ */
+require './../models/FuncionesBePickler.php';
+require './InfoSolicitadaBy.php';
+/**
+ * Variables Utilizadas
+ */
+$solicitadoBy= '';
+$idtblproductdetalle='';
+$cantidad  ='';
+$resultado= '';
+/**
+ * Validamos que el array $_POST no es null.
+ */
+if (!empty($_POST)){
+
+	$solicitadoBy=$_POST["solicitadoBy"];
+    $idtblproductdetalle=$_POST["idtblproductdetalle"];
+    $cantidad=$_POST["cantidad"];
+    $casoAddDiss=$_POST["casoAddDiss"];
+    /**
+     * Mandamos los parámetros y llamamos a la función que ejecutara la sentencia y retorna el resultado.
+     */
+    $resultado = FuncionesBePickler::setUpdateStockTblproductdetalle($idtblproductdetalle,$cantidad,$casoAddDiss);
+
+    if($resultado)
+    {
+        /**
+         * Si es éxitos le mandamos los resultados a quien lo solicito.
+         */
+    	InfoSolicitadaBy::solicitadaby($solicitadoBy, $resultado);
+
+    }else
+    {
+        /**
+         * Si fallo manda a la función de fallo a quien lo solicito.
+         */
+    	InfoSolicitadaBy::sinDatos($solicitadoBy);
+    }
+}
+/**
+ * Desctruimos las variables para liberar memoria
+ */
+unset($solicitadoBy);
+unset($idtblproductdetalle);
+unset($cantidad);
+unset($resultado);
+?>
