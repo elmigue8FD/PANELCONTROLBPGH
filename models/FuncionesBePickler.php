d6150473c2264530639ff4cc1e9175d1ec13ea6e
reynavazquez <reyna.martinez@mountditech.com>
2017-05-25 13:32:22 -0500

    funciones repetidas eliminadas SP

--- models/FuncionesBePickler.php
+++ models/FuncionesBePickler.php
@@ -11922,70 +11922,10 @@ AND exists
 		}
 	}
 	
-	/*Obtiene un registro de tblentregaproducto  por ordencompra y proveedor 2*/
-    public static function getTblentregaproductoByOrdenProveedorFechas($idtblordencompra,$idtblproveedor){
-	    
-		$consulta = "SELECT * FROM tblentregaproducto WHERE tblentregaproducto_status='entregado'
-                      AND tblentregaproducto_statusdeposito='pagado' AND
-                 tblentregaproducto_idtblordencompra = ? AND tblentregaproducto_idtblproveedor = ? "; 
-		
-		try{
-
-			$resultado = ConexionDB::getInstance()->getDb()->prepare($consulta);
-			$resultado->bindParam(1,$idtblordencompra,PDO::PARAM_INT);
-			$resultado->bindParam(2,$idtblproveedor,PDO::PARAM_INT);
-			$resultado->execute();
-			return $resultado->fetchAll(PDO::FETCH_ASSOC); //retorna los campos del registro 
-		} catch(PDOException $e){
-			return false;
-		}
-	}
 	
-	/*Consultar un carritoproduct por idtblordencompra y proveedor */
-	public static function getAllTblcarritoproductByidorden3($idtblordencompra,$idtblproveedor){
-	    
-		$consulta = "SELECT TCP.tblcarritoproduct_cantidad,TCP.tblcarritoproduct_nombreproducto,
-                     TCP.tblcarritoproduct_precioreal,TCP.tblcarritoproduct_preciobp,I.tblespecificingrediente_nombre,
-                     TPD.tblproductdetalle_diametro,TPD.tblproductdetalle_largo,TPD.tblproductdetalle_ancho,TPD.tblproductdetalle_piezas,CA.tblcategproduct_nombre
-                     FROM tblcarritoproduct TCP		             
-       				 INNER JOIN tblordencompra TOC ON  TCP.tblcarritoproduct_idtblordencompra = TOC.idtblordencompra  
-      				 INNER JOIN tblproductdetalle TPD ON TPD.idtblproductdetalle = TCP.tblcarritoproduct_idtblproductdetalle
-      				 INNER JOIN tblespecificingrediente I ON TPD.tblespecificingrediente_idtblespecificingrediente = I.idtblespecificingrediente
-				 INNER JOIN tblproducto TP ON TP.idtblproducto = TPD.tblproducto_idtblproducto      				 
-				 INNER JOIN tblcategproduct CA ON TP.tblcategproduct_idtblcategproduct = CA.idtblcategproduct
-				 WHERE TCP.tblcarritoproduct_idtblordencompra = ? AND TP.tblproveedor_idtblproveedor = ? ";
-		
-		try{
-
-			$resultado = ConexionDB::getInstance()->getDb()->prepare($consulta);
-			$resultado->bindParam(1,$idtblordencompra,PDO::PARAM_INT);
-			$resultado->bindParam(2,$idtblproveedor,PDO::PARAM_INT);
-			$resultado->execute();
-			return $resultado->fetchAll(PDO::FETCH_ASSOC); //retorna los campos del registro 
-		} catch(PDOException $e){
-			return false;
-		}
-	}
 	
-	/*---------reporte de productos complementarios*/
-	public static function getAllTblordencompraProCompR($idtblordencompra,$idtblproveedor){	    
-		$consulta = "SELECT OC.*,ENV.*,PC.*,PCOM.* FROM tblcarritoproductcomplem PCOM  
-           INNER JOIN tblordencompra OC ON PCOM.tblcarritoproductcomplem_idtblordencompra = OC.idtblordencompra
-		   INNER JOIN tbldatosenvio ENV ON ENV.tbldatosenvio_idtblordencompra = OC.idtblordencompra
-		   INNER JOIN  tblproductcomplem PC ON PC.idtblproductcomplem = PCOM.tblcarritoproductcomplem_idtblproductcomplem
-      	     WHERE OC.idtblordencompra= ? AND PC.tblproveedor_idtblproveedor=?";
-		   
-		try{   
-
-			$resultado = ConexionDB::getInstance()->getDb()->prepare($consulta);
-			$resultado->bindParam(1,$idtblordencompra,PDO::PARAM_INT);
-			$resultado->bindParam(2,$idtblproveedor,PDO::PARAM_INT);
-			$resultado->execute();
-			return $resultado->fetchAll(PDO::FETCH_ASSOC); //retorna los campos del registro 
-		} catch(PDOException $e){
-			return false;
-		}
-	}
+	
+	
 	
 	//-----
 }
