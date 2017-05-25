cb97f2043a4f02d917a52b10fc9f76cf42f26766
reynavazquez <reyna.martinez@mountditech.com>
2017-05-25 13:23:59 -0500

    funcion repetida eliminada

--- models/FuncionesBePickler.php
+++ models/FuncionesBePickler.php
@@ -11659,34 +11659,7 @@ AND exists
 		}
     }
 
-     /*Muestra orden pagada en base a un rango d efachas*/
-	public static function getAllTblordencompraDatosbyFechas($nameCiudad,$fechaIni,$fechaFin){ 
-	                       
-			$consulta = "SELECT Distinct(PRO.tblproveedor_nombre),PRO.idtblproveedor,
-                        OC.*,ENV.*,DC.tblhistcupondescuento_descuento
-                        FROM tblordencompra OC                                        
-   INNER JOIN tblcarritoproduct CA ON  CA.tblcarritoproduct_idtblordencompra = OC.idtblordencompra 
-   INNER JOIN tbldatosenvio ENV ON ENV.tbldatosenvio_idtblordencompra = OC.idtblordencompra
-   INNER JOIN tblproductdetalle TPD ON TPD.idtblproductdetalle = CA.tblcarritoproduct_idtblproductdetalle
-   INNER JOIN tblproducto TP ON TP.idtblproducto = TPD.tblproducto_idtblproducto
-   INNER JOIN tblproveedor PRO ON PRO.idtblproveedor = TP.tblproveedor_idtblproveedor
-   LEFT JOIN tblhistcupondescuento DC ON DC.tblhistcupondescuento_idtblordencompra =OC.idtblordencompra     
-   WHERE OC.tblordencompra_statuspagado=1 AND ENV.tbldatosenvio_ciudad=? 
-   AND OC.tblordencompra_fchordencompra Between ? AND ?";
-   	  	
-				
-		try{
-
-			$resultado = ConexionDB::getInstance()->getDb()->prepare($consulta);
-			$resultado->bindParam(1,$nameCiudad,PDO::PARAM_STR);
-            $resultado->bindParam(2,$fechaIni,PDO::PARAM_STR);
-            $resultado->bindParam(3,$fechaFin,PDO::PARAM_STR);			
-			$resultado->execute();
-			return $resultado->fetchAll(PDO::FETCH_ASSOC); //retorna los campos del registro 
-		} catch(PDOException $e){
-			return false;
-		}
-	}
+    
 	
 	/*Obtiene un registro de tblentregaproducto  por ordencompra y proveedor 2*/
     public static function getTblentregaproductoByOrdenProveedorFechas($idtblordencompra,$idtblproveedor){
@@ -11920,6 +11893,7 @@ AND exists
 	
 	//------
 	 /*Muestra orden pagada en base a un rango d efachas*/
+	  
 	public static function getAllTblordencompraDatosbyFechas($nameCiudad,$fechaIni,$fechaFin){ 
 	                       
 			$consulta = "SELECT Distinct(PRO.tblproveedor_nombre),PRO.idtblproveedor,
